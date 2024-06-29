c7c6988d0042d9a1af6a81d69aa0ed51e37c634e
Benoit SOMMERVOGEL <benoit.sommervogel@jobteaser.com>
2024-06-29 16:02:39 +0200

    made a second attempt to fix the overlaping slots issue

--- lib/slot_machine/models/user.rb
+++ lib/slot_machine/models/user.rb
@@ -4,12 +4,15 @@ require "slot_machine/models/calendar"
 
 module SlotMachine
 	class User
+
+		# CALENDAR JUST FOR DEBUG, TO DELETE
+		attr_accessor :name
 		def initialize(name)
 			@name = name
 			@calendar = SlotMachine::Calendar.new(name)
 		end
 
-		def get_avalaible_slots(start_time, end_time, duration)
+		def get_available_slots(start_time, end_time, duration)
 			return @calendar.get_available_slots(start_time, end_time, duration)
 		end
 	end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -2,17 +2,43 @@
 require "slot_machine/models/calendar"
 
 RSpec.describe SlotMachine::Calendar do
-  let!(:calendar) {SlotMachine::Calendar.new("andy")}
-  let(:start_time) {"2022-08-02T9:00:00+02:00"}
-  let(:end_time) {"2022-08-02T18:00:00+02:00"}
 
   context "When it receives a file that exists" do
+    let!(:calendar) {SlotMachine::Calendar.new("andy")}
+    let(:start_time) {"2022-08-01T9:00:00+02:00"}
+    let(:end_time) {"2022-08-01T18:00:00+02:00"}
+    let(:start_time_busy_slots) {
+      ["2022-08-01 09:00:00 +0200",
+      "2022-08-02 09:00:00 +0200",
+      "2022-08-02 15:00:00 +0200",
+      "2022-08-03 09:00:00 +0200",
+      "2022-08-04 10:00:00 +0200",
+      "2022-08-04 16:00:00 +0200"]}
+    
+    let(:start_time_available_slots) {
+      ["2022-08-01T14:00:00+02:00",
+      "2022-08-01T15:00:00+02:00",
+      "2022-08-01T16:00:00+02:00"]}
+  
     it "parses it and fill a linked list" do
-      expect(calendar.get_calendar_size).to eq(6)
+      expect(calendar.get_slot_array).to eq(start_time_busy_slots)
     end
 
     it "can return the available slots for the day" do
-      expect(calendar.get_available_slots(start_time, end_time, 1).size).to eq(6)
+      expect(calendar.get_available_slots(start_time, end_time, 2)).to eq(start_time_available_slots)
+    end
+  end
+
+  context "When it receives a NIGHTMARISH file" do
+    let!(:calendar) {SlotMachine::Calendar.new("lucifer")}
+    let(:start_time_busy_slots) {
+      ["2022-08-01 09:00:00 +0200",
+      "2022-08-01 12:00:00 +0200",
+      "2022-08-01 16:00:00 +0200",
+      "2022-08-02 12:00:00 +0200"]}
+
+    it "parses it and fill a linked list" do
+      expect(calendar.get_slot_array).to eq(start_time_busy_slots)
     end
   end
 end
